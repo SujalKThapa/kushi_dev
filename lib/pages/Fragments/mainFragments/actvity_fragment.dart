b58bbcc977889144f5cc200761a84da9df6a69ea
Suhas181 <suhasswamy6756@gmail.com>
2024-04-30 22:47:41 +0530

    ready make new repo

--- lib/pages/Fragments/mainFragments/actvity_fragment.dart
+++ lib/pages/Fragments/mainFragments/actvity_fragment.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:kushi_3/components/bar_graph/bar_graph.dart';
 
-class ActivityPage extends StatefulWidget {
-  const ActivityPage({super.key});
+class ActivityFragment extends StatefulWidget {
+  const ActivityFragment({super.key});
 
   @override
-  State<ActivityPage> createState() => _ActivityPageState();
+  State<ActivityFragment> createState() => _ActivityFragmentState();
 }
 
-class _ActivityPageState extends State<ActivityPage> {
+class _ActivityFragmentState extends State<ActivityFragment> {
   List<double> weeklySummary = [4.40, 2.50, 42.42, 30, 50, 96, 59];
 
   @override
